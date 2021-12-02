a752696a961e82fbe1a33555a004c2bb0b8099d4
Pedro Marques <pedromar2010@gmail.com>
2021-12-02 23:06:27 +0000

    completed correlation analysis

---
+++ lab01_data_profiling/set2_correlation_analysis.py
@@ -0,0 +1,20 @@
+from pandas import read_csv
+from pandas.plotting import register_matplotlib_converters
+from matplotlib.pyplot import subplots, savefig, show
+from ds_charts import get_variable_types, HEIGHT
+from matplotlib.pyplot import figure, savefig, show, title
+from seaborn import heatmap
+
+register_matplotlib_converters()
+filename = "lab01_data_profiling/data/set2_air_quality_tabular.csv"
+data = read_csv(filename, index_col='date', parse_dates=True, infer_datetime_format=True)
+
+corr_mtx = data.corr()
+print(corr_mtx)
+
+fig = figure(figsize=[15, 15])
+
+heatmap(abs(corr_mtx), xticklabels=corr_mtx.columns, yticklabels=corr_mtx.columns, annot=True, cmap='Blues', fmt='.2f')
+title('Correlation analysis')
+savefig(f'lab01_data_profiling/images/correlation_analysis_set2.png')
+show()
