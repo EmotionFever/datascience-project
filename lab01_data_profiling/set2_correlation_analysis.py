728341e60052fab283766e7eda189fbc417ae4a5
Pedro Marques <pedromar2010@gmail.com>
2021-12-06 20:59:50 +0000

    added extra code to correlation

--- lab01_data_profiling/set2_correlation_analysis.py
+++ lab01_data_profiling/set2_correlation_analysis.py
@@ -12,6 +12,18 @@ data = read_csv(filename, index_col='date', parse_dates=True, infer_datetime_for
 corr_mtx = data.corr()
 print(corr_mtx)
 
+correlations = []
+for row in corr_mtx.index.values:
+    for column in corr_mtx.columns:
+        mod_r = corr_mtx.loc[row][column]
+        if row != column and corr_mtx.loc[row][column] > 0.7:
+            correlations.append((row, column, round(mod_r, 2)))
+correlations.sort(key=lambda x:x[2])
+for correlation in correlations:
+    row = correlation[0]
+    column = correlation[1]
+    mod_r = correlation[2]
+    print("(" + row + ", " + column + ", |r| = " + str(mod_r) +"), ", end="")
 fig = figure(figsize=[15, 15])
 
 heatmap(abs(corr_mtx), xticklabels=corr_mtx.columns, yticklabels=corr_mtx.columns, annot=True, cmap='Blues', fmt='.2f')
