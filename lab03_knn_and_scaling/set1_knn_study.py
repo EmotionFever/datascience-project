92231bf7b4e72a5057d6965070cb2ee3986691ca
Pedro Marques <pedromar2010@gmail.com>
2021-12-10 15:07:42 +0000

    started set 1 balancing with over sample

---
+++ lab03_knn_and_scaling/set1_knn_study.py
@@ -0,0 +1,54 @@
+from numpy import ndarray
+from pandas import DataFrame, read_csv, unique
+from matplotlib.pyplot import figure, savefig, show
+from sklearn.neighbors import KNeighborsClassifier
+from ds_charts import plot_evaluation_results, multiple_line_chart
+from sklearn.metrics import accuracy_score
+
+file_tag = 'set1'
+filename = 'lab03_knn_and_scaling/ew_data/set1'
+target = 'PERSON_INJURY'
+
+train: DataFrame = read_csv(f'{filename}_train.csv')
+trnY: ndarray = train.pop(target).values
+trnX: ndarray = train.values
+labels = unique(trnY)
+labels.sort()
+
+test: DataFrame = read_csv(f'{filename}_test.csv')
+tstY: ndarray = test.pop(target).values
+tstX: ndarray = test.values
+
+nvalues = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+dist = ['manhattan', 'euclidean', 'chebyshev']
+values = {}
+best = (0, '')
+last_best = 0
+for d in dist:
+    yvalues = []
+    for n in nvalues:
+        knn = KNeighborsClassifier(n_neighbors=n, metric=d)
+        knn.fit(trnX, trnY)
+        prdY = knn.predict(tstX)
+        yvalues.append(accuracy_score(tstY, prdY))
+        if yvalues[-1] > last_best:
+            best = (n, d)
+            last_best = yvalues[-1]
+    values[d] = yvalues
+
+figure()
+multiple_line_chart(nvalues, values, title='KNN variants', xlabel='n', ylabel='accuracy', percentage=True)
+savefig('images/{file_tag}_knn_study.png')
+show()
+print('Best results with %d neighbors and %s'%(best[0], best[1]))
+
+"""
+clf = knn = KNeighborsClassifier(n_neighbors=best[0], metric=best[1])
+clf.fit(trnX, trnY)
+prd_trn = clf.predict(trnX)
+prd_tst = clf.predict(tstX)
+plot_evaluation_results(labels, trnY, prd_trn, tstY, prd_tst)
+savefig('images/{file_tag}_knn_best.png')
+show()
+
+"""
