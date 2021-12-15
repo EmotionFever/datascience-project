4492c724b85132005f0bd76113258af514e03b1a
emiliale <emila.lebiedowska@gmail.com>
2021-12-15 12:44:46 +0000

    Added balancing for set2

---
+++ lab04_naive_bayes_and_balancing/set2_balancing.py
@@ -0,0 +1,72 @@
+from pandas import read_csv
+from matplotlib.pyplot import figure, savefig, show
+from ds_charts import bar_chart
+
+filename = 'lab03_knn_and_scaling/ew_data/set2_train_scaled.csv'
+file = "set2"
+original = read_csv(filename, sep=',', decimal='.')
+class_var = 'ALARM'
+target_count = original[class_var].value_counts()
+positive_class = target_count.idxmin()
+negative_class = target_count.idxmax()
+#ind_positive_class = target_count.index.get_loc(positive_class)
+print('Minority class=', positive_class, ':', target_count[positive_class])
+print('Majority class=', negative_class, ':', target_count[negative_class])
+print('Proportion:', round(target_count[positive_class] / target_count[negative_class], 2), ': 1')
+values = {'Original': [target_count[positive_class], target_count[negative_class]]}
+
+figure()
+bar_chart(target_count.index, target_count.values, title='Class balance')
+savefig(f'lab04_naive_bayes_and_balancing/images/{file}/{file}_balance.png')
+show()
+
+df_positives = original[original[class_var] == positive_class]
+df_negatives = original[original[class_var] == negative_class]
+
+from pandas import concat, DataFrame
+
+df_neg_sample = DataFrame(df_negatives.sample(len(df_positives)))
+df_under = concat([df_positives, df_neg_sample], axis=0)
+df_under.to_csv(f'lab04_naive_bayes_and_balancing/ew_data/{file}_under.csv', index=False)
+values['UnderSample'] = [len(df_positives), len(df_neg_sample)]
+print('Minority class=', positive_class, ':', len(df_positives))
+print('Majority class=', negative_class, ':', len(df_neg_sample))
+print('Proportion:', round(len(df_positives) / len(df_neg_sample), 2), ': 1')
+
+
+from pandas import concat, DataFrame
+
+df_pos_sample = DataFrame(df_positives.sample(len(df_negatives), replace=True))
+df_over = concat([df_pos_sample, df_negatives], axis=0)
+df_over.to_csv(f'lab04_naive_bayes_and_balancing/ew_data/{file}_over.csv', index=False)
+values['OverSample'] = [len(df_pos_sample), len(df_negatives)]
+print('Minority class=', positive_class, ':', len(df_pos_sample))
+print('Majority class=', negative_class, ':', len(df_negatives))
+print('Proportion:', round(len(df_pos_sample) / len(df_negatives), 2), ': 1')
+
+
+from pandas import Series
+from imblearn.over_sampling import SMOTE
+RANDOM_STATE = 42
+
+smote = SMOTE(sampling_strategy='minority', random_state=RANDOM_STATE)
+y = original.pop(class_var).values
+X = original.values
+smote_X, smote_y = smote.fit_resample(X, y)
+df_smote = concat([DataFrame(smote_X), DataFrame(smote_y)], axis=1)
+df_smote.columns = list(original.columns) + [class_var]
+df_smote.to_csv(f'lab04_naive_bayes_and_balancing/ew_data/{file}_smote.csv', index=False)
+
+smote_target_count = Series(smote_y).value_counts()
+values['SMOTE'] = [smote_target_count[positive_class], smote_target_count[negative_class]]
+print('Minority class=', positive_class, ':', smote_target_count[positive_class])
+print('Majority class=', negative_class, ':', smote_target_count[negative_class])
+print('Proportion:', round(smote_target_count[positive_class] / smote_target_count[negative_class], 2), ': 1')
+
+from matplotlib.pyplot import figure, show
+from ds_charts import multiple_bar_chart
+
+figure()
+multiple_bar_chart([positive_class, negative_class], values, title='Target', xlabel='frequency', ylabel='Class balance')
+savefig(f'lab04_naive_bayes_and_balancing/images/{file}/{file}_all_balances.png')
+show()
